67ab1e64a87eaa5dda86c784f0ca8e2c169c06ea
Jepps42 <63672521+Jepps42@users.noreply.github.com>
2026-03-29 15:20:58 -0400

    Updated camera controller

--- Assets/Scripts/CharacterController/CameraController.cs
+++ Assets/Scripts/CharacterController/CameraController.cs
@@ -2,16 +2,54 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //Declaring some variables
-    public Transform FollowTarget, LookTarget;
-    public float FollowSpeed = 10f;
+    public bool debugs = true;
+    Vector3 myLook;
+    float lookSpeed = 100f;
+    [SerializeField] private Camera myCam;
+    [SerializeField] private float camLock = 90f;
+    float onStartTimer;
 
-    //Using LateUpdate so the camera can follow the player once all the movements happened on the current frame
-    private void LateUpdate()
+    //[SerializeField] GameObject GameManager;
+
+    void Awake()
     {
-        Vector3 targetPosition = FollowTarget.position;
-        transform.position = Vector3.Lerp(transform.position, targetPosition, FollowSpeed * Time.deltaTime);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        myLook = transform.localEulerAngles;
+    }
 
-        transform.LookAt(LookTarget);
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        onStartTimer += Time.deltaTime;
+        myLook += DeltaLook() * Time.deltaTime;
+        myLook.y = Mathf.Clamp(myLook.y, -camLock, camLock);
+
+        //Debug.Log("myLook: " + myLook);
+        transform.rotation = Quaternion.Euler(0f, myLook.x, 0f);
+        myCam.transform.rotation = Quaternion.Euler(-myLook.y, myLook.x, 0f);
+
+        if (debugs)
+        {
+            Debug.DrawRay(myCam.transform.position, myCam.transform.forward * 10f, Color.black);
+        }
+    }
+
+    Vector3 DeltaLook()
+    {
+        Vector3 dLook;
+        float rotY = Input.GetAxis("Mouse Y") * lookSpeed;
+        float rotX = Input.GetAxis("Mouse X") * lookSpeed;
+        dLook = new Vector3(rotX, rotY, 0f);
+        if (dLook != Vector3.zero)
+        {
+            //Debug.Log("delta look: " + dLook);
+        }
+        if (onStartTimer < 1f)
+        {
+            dLook = Vector3.ClampMagnitude(dLook, onStartTimer * 10f);
+        }
+
+        return dLook;
     }
 }
